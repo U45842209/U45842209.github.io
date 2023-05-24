0c230368490cec87af6217be4f27938a9bf315ea
U45842209 <marc.tahon.dujardin.bis@gmail.com>
2023-05-24 14:34:33 +0200

    Update

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function redirectPage(code) {
   switch (code) {
     case 'page1':
-      loadElements("réseau");
+      window.open('/Network', '_blank');
       break;
     case 'page2':
-      loadElements("télécommunication");
+      window.open('/Telecom', '_blank');
       break;
     default:
       console.log('Invalid code. No matching page found.');
   }
 }
 
-function loadElements(category) {
+function loadElements() {
   var myObject = {
     "https://github.com/U45842209/U45842209": "main",
     "https://github.com/U45842209/SAE11": "réseau",
@@ -19,17 +19,26 @@ function loadElements(category) {
     "https://github.com/U45842209/Vis-ma-Vie_Fibre-optique": "télécommunication",
     "https://github.com/U45842209/Formation_Comprendre-et-mettre-en-uvre-le-protocole-SIP-dans-le-cadre-de-la-VoIP-et-ToIP": "télécommunication",
     "/Telecom/Ressources/SAE31.pdf": "télécommunication",
-    "https://github.com/U45842209/Reseaux-cellulaires": "télécommunication",
+    "https://github.com/U45842209/Reseaux-cellulaires": "télécommunication"
   };
 
-  var filteredElements = Object.keys(myObject).filter(function(key) {
-    return myObject[key] === category || myObject[key] === "main";
-  });
+  if (window.location.pathname === '/Network') {
+    var links = document.querySelectorAll('.projects ul a');
+    var filteredLinks = [];
 
-  // Load the elements
-  for (var i = 0; i < filteredElements.length; i++) {
-    var element = filteredElements[i];
-    console.log("Loading element:", element);
-    // Code to load the element goes here
+    for (var i = 0; i < links.length; i++) {
+      var link = links[i];
+      var href = link.getAttribute('href');
+      if (myObject[href] === 'réseau') {
+        filteredLinks.push(link);
+      }
+    }
+
+    // Load the filtered links
+    for (var j = 0; j < filteredLinks.length; j++) {
+      var filteredLink = filteredLinks[j];
+      console.log("Loading element:", filteredLink.href);
+      // Code to load the element goes here
+    }
   }
 }
