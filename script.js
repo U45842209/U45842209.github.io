784a2e86576d87662a76a25853630b271f0a6f2b
U45842209 <marc.tahon.dujardin.bis@gmail.com>
2023-05-24 15:25:00 +0200

    Update

--- script.js
+++ script.js
@@ -22,53 +22,55 @@ function loadElements() {
     "https://github.com/U45842209/Reseaux-cellulaires": { title: "Reseaux cellulaires", type: "télécommunication" }
   };
 
+function loadElements() {
+  var myObject = [
+    { url: "https://github.com/U45842209/U45842209", title: "A propos de moi", type: "main" },
+    { url: "https://github.com/U45842209/SAE11", title: "SAE11 : Hygiène informatique et cybersécurité", type: "réseau" },
+    { url: "https://github.com/U45842209/SAE24", title: "SAE24 : Mise en place d'un réseau d'entreprise", type: "réseau" },
+    { url: "https://github.com/U45842209/Vis-ma-Vie_Fibre-optique", title: "Intervention pose de fibre", type: "télécommunication" },
+    { url: "https://github.com/U45842209/Formation_Comprendre-et-mettre-en-uvre-le-protocole-SIP-dans-le-cadre-de-la-VoIP-et-ToIP", title: "Formation : Comprendre et mettre en œuvre le protocole SIP dans le cadre de la VoIP et ToIP", type: "télécommunication" },
+    { url: "/Telecom/Ressources/SAE31.pdf", title: "SAE31 : Analyse des filtres en SuperCollider", type: "réseau" },
+    { url: "https://github.com/U45842209/Reseaux-cellulaires", title: "Reseaux cellulaires", type: "réseau" }
+  ];
+
   var ul = document.querySelector('.projects ul');
 
   // Clear existing list items
-  while (ul.firstChild) {
-    ul.removeChild(ul.firstChild);
-  }
+  ul.innerHTML = "";
 
   // Create the "main" element if it doesn't exist
-  if (!myObject.hasOwnProperty("https://github.com/U45842209/U45842209")) {
-    var mainURL = "https://github.com/U45842209/U45842209";
-    var mainTitle = "A propos de moi";
-    var mainType = "main";
-    var mainLi = createListItem(mainURL, mainTitle, mainType);
+  var mainElement = myObject.find(element => element.type === "main");
+  if (mainElement) {
+    var mainLi = createListItem(mainElement.url, mainElement.title);
     ul.appendChild(mainLi);
   }
 
   // Get the current page URL
   var currentPageURL = window.location.href;
 
-  // Iterate over the elements in myObject and load the corresponding elements based on the current page URL
-  for (var url in myObject) {
-    var title = myObject[url].title;
-    var type = myObject[url].type;
-
-    if (currentPageURL.includes("/Network") && type === "réseau") {
-      var li = createListItem(url, title, type);
-      ul.appendChild(li);
+  // Filter elements based on the current page URL
+  var filteredElements = myObject.filter(element => {
+    if (currentPageURL.includes("/Network")) {
+      return element.type === "réseau";
     } else if (currentPageURL.includes("/Telecom")) {
-      var li = createListItem(url, title, type);
-      ul.appendChild(li);
+      return element.type === "réseau" || element.type === "télécommunication";
     }
-  }
+    return false;
+  });
+
+  // Create list items for the filtered elements
+  filteredElements.forEach(element => {
+    var li = createListItem(element.url, element.title);
+    ul.appendChild(li);
+  });
 }
 
-function createListItem(url, title, type) {
+function createListItem(url, title) {
   var li = document.createElement('li');
   var a = document.createElement('a');
   a.classList.add('folder');
   a.href = url;
-  a.textContent = title;
+  a.textContent = title + " : ";
   li.appendChild(a);
-
-  if (type === "réseau") {
-    li.classList.add('reseau');
-  } else if (type === "télécommunication") {
-    li.classList.add('telecommunication');
-  }
-
   return li;
 }
