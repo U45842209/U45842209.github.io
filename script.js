3eece13b551a864013b0e5334c485de890a8e7d5
U45842209 <marc.tahon.dujardin.bis@gmail.com>
2023-05-24 14:57:16 +0200

    Update

--- script.js
+++ script.js
@@ -13,26 +13,47 @@ function redirectPage(code) {
 
 function loadElements() {
   var myObject = {
-    "https://github.com/U45842209/U45842209": "main",
-    "https://github.com/U45842209/SAE11": "réseau",
-    "https://github.com/U45842209/SAE24": "réseau",
-    "https://github.com/U45842209/Vis-ma-Vie_Fibre-optique": "télécommunication",
-    "https://github.com/U45842209/Formation_Comprendre-et-mettre-en-uvre-le-protocole-SIP-dans-le-cadre-de-la-VoIP-et-ToIP": "télécommunication",
-    "/Telecom/Ressources/SAE31.pdf": "télécommunication",
-    "https://github.com/U45842209/Reseaux-cellulaires": "télécommunication"
+    "https://github.com/U45842209/U45842209": { title: "A propos de moi", type: "main" },
+    "https://github.com/U45842209/SAE11": { title: "SAE11 : Hygiène informatique et cybersécurité", type: "réseau" },
+    "https://github.com/U45842209/SAE24": { title: "SAE24 : Mise en place d'un réseau d'entreprise", type: "réseau" },
+    "https://github.com/U45842209/Vis-ma-Vie_Fibre-optique": { title: "Intervention pose de fibre", type: "télécommunication" },
+    "https://github.com/U45842209/Formation_Comprendre-et-mettre-en-uvre-le-protocole-SIP-dans-le-cadre-de-la-VoIP-et-ToIP": { title: "Formation : Comprendre et mettre en œuvre le protocole SIP dans le cadre de la VoIP et ToIP", type: "télécommunication" },
+    "/Telecom/Ressources/SAE31.pdf": { title: "SAE31 : Analyse des filtres en SuperCollider", type: "télécommunication" },
+    "https://github.com/U45842209/Reseaux-cellulaires": { title: "Reseaux cellulaires", type: "télécommunication" }
   };
 
-  var ul = document.querySelector('.projects ul');
+  // Create the "main" element
+  var mainURL = "https://github.com/U45842209/U45842209";
+  var mainTitle = "A propos de moi";
+  var mainType = "main";
+  var mainLi = createListItem(mainURL, mainTitle, mainType);
 
+  // Append the "main" element to the beginning of the list
+  var ul = document.querySelector('.projects ul');
+  ul.insertBefore(mainLi, ul.firstChild);
+
+  // Iterate over the remaining elements in myObject and load the elements with titles
   for (var url in myObject) {
-    if (myObject[url] === "réseau") {
-      var li = document.createElement('li');
-      var a = document.createElement('a');
-      a.classList.add('folder');
-      a.href = url;
-      a.textContent = myObject[url] + " : ";
-      li.appendChild(a);
-      ul.appendChild(li);
-    }
+    var title = myObject[url].title;
+    var type = myObject[url].type;
+    var li = createListItem(url, title, type);
+    ul.appendChild(li);
   }
+}
+
+function createListItem(url, title, type) {
+  var li = document.createElement('li');
+  var a = document.createElement('a');
+  a.classList.add('folder');
+  a.href = url;
+  a.textContent = title + " : ";
+  li.appendChild(a);
+
+  if (type === "réseau") {
+    li.classList.add('reseau');
+  } else if (type === "télécommunication") {
+    li.classList.add('telecommunication');
+  }
+
+  return li;
 }
